eb291e45bc9922ef5250e5bc18db491314ed182c
jojaehyun <jojae38@naver.com>
2026-02-10 17:57:24 +0900

    틀 제작

---
+++ 6.program/python/main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    # app = QApplication([])
+
+    # with open(resource_path("style.qss"), "r", encoding="utf-8") as f:
+    #     qss = f.read()
+    #     app.setStyleSheet(qss)
+
+    # window = MyApp()
+    # window.show()
+    # app.exec()
